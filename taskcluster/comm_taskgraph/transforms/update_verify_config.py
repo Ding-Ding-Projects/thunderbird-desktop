f067208f5c6ef4db2546f2a64364920b1c751d68
Daniel Darnell <daniel@thunderbird.net>
2026-07-16 16:41:00 -0700

    Revert "Bug 2052985 - Follow-up: fix release-level param inn update_verify_config.py"
    This reverts commit 55bea702876add904d02083a659a79f84fc9273a.

--- taskcluster/comm_taskgraph/transforms/update_verify_config.py
+++ taskcluster/comm_taskgraph/transforms/update_verify_config.py
@@ -119,9 +119,7 @@ def add_command(config, tasks):
                 platform=task["attributes"]["build_platform"],
                 **{
                     "release-type": config.params["release_type"],
-                    "release-level": release_level(
-                        config.graph_config["release-branches"], config.params
-                    ),
+                    "release-level": release_level(config.params),
                 },
             )
             # ignore things that resolved to null
